f2489df112e896a17798882ce1a55d506e0a0d5f
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-27 09:12:12 -0400

    Part 2 of Lab 5 Unit 5

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,5 +1,24 @@
-public class Main {
+import java.util.Scanner;
+
+public class BirthMonth {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int month = 0;
+
+
+
+        System.out.println("What is your birth month?");
+        month = scan.nextInt();
+
+        if(month <= 12 && month >= 1){
+            System.out.println("Your birth month is " + month + ".");
+        }
+        else {
+            System.out.println("You have entered an incorrect birth month. Please try again.");
+            System.exit(0);
+    }
+
+
     }
 }
